b34414c74a8fbb4bb638ecc6f6b3927c7c0d2579
KOH <gaosheng087@gmail.com>
2022-02-19 15:28:38 +0900

    add table & add login

--- database/migrations/2022_02_19_052401_create_posts_table.php
+++ database/migrations/2022_02_19_052401_create_posts_table.php
@@ -15,6 +15,12 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('body');
+            $table->unsignedBigInteger('user_id');
+            // 長い桁の数値を保存できる
+            $table->foreign('user_id')->references('id')->on('users');
+            // 外部テーブルのuser_idを参照する(foreignキー)
             $table->timestamps();
         });
     }
