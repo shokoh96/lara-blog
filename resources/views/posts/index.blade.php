936597dc5915fb5f9133c5d04d3788e28dea766b
KOH <gaosheng087@gmail.com>
2022-03-01 19:36:46 +0900

    編集

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,33 +1,33 @@
 @extends('layouts.app_original')
 
 @section('content')
-<div class="container mt-5">
-  <div class="row justify-content-center">
-    <div class="col-md-8">
-      <div class="card text-center">
-        <div class="card-header">
-          投稿一覧
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card text-center">
+                    <div class="card-header">
+                        投稿一覧
+                    </div>
+                    @foreach ($posts as $post)
+                        <div class="card-body">
+                            <h5 class="card-title">タイトル ： {{ $post->title }}</h5>
+                            <p class="card-text">
+                                内容 : {{ $post->body }}
+                            </p>
+                            <p class="card-text">投稿者： {{ $post->user->name }}さん</p>
+                            <a href="{{ route('posts.show', $post->id) }}" class="btn btn-primary">詳細へ</a>
+                        </div>
+                        <div class="card-footer text-muted">
+                            投稿日時 ： {{ $post->created_at }}
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+            <div class="col-md-2">
+                <a href="{{ route('posts.create') }}" class="btn btn-primary">
+                    新規投稿
+                </a>
+            </div>
         </div>
-        @foreach($posts as $post)
-        <div class="card-body">
-          <h5 class="card-title">タイトル ： {{ $post->title }}</h5>
-          <p class="card-text">
-            内容 : {{ $post->body }}
-          </p>
-          <p class="card-text">投稿者： {{ $post->user->name }}さん</p>
-          <a href="{{ route('posts.show', $post->id) }}" class="btn btn-primary">詳細へ</a>
-        </div>
-        <div class="card-footer text-muted">
-          投稿日時 ： {{ $post->created_at }}
-        </div>
-        @endforeach
-      </div>
     </div>
-    <div class="col-md-2">
-      <a href="{{ route('posts.create') }}" class="btn btn-primary">
-        新規投稿
-      </a>
-    </div>
-  </div>
-</div>
 @endsection
